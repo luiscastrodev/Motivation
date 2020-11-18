3208e00468a8a8802c8ed7c27058b2e33a1cf002
luis <luiscastrodev@gmail.com>
2020-11-18 19:14:39 -0300

    First Commit
    Estrutura projeto inicial

---
+++ app/src/main/java/br/com/motivation/SplashActvity.kt
@@ -0,0 +1,11 @@
+package br.com.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActvity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_actvity)
+    }
+}
