8f42e2baaa094f16cca53d5a4fc34f9047ccdae7
luis <luiscastrodev@gmail.com>
2020-11-18 19:47:51 -0300

    criacao SharedPreference

--- app/src/main/java/br/com/motivation/ui/MainActivity.kt
+++ app/src/main/java/br/com/motivation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package br.com.motivation
+package br.com.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.motivation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/br/com/motivation/ui/SplashActvity.kt
+++ app/src/main/java/br/com/motivation/ui/SplashActvity.kt
@@ -1,22 +1,30 @@
-package br.com.motivation
+package br.com.motivation.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import br.com.motivation.R
+import br.com.motivation.infra.SecurityPreferences
 import kotlinx.android.synthetic.main.activity_splash_actvity.*
 
 class SplashActvity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var  mSecurityPreferences : SecurityPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_actvity)
+
+        mSecurityPreferences = SecurityPreferences(this)
 
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
 
         buttonSave.setOnClickListener(this)
+
     }
 
     override fun onClick(v: View) {
@@ -30,6 +38,7 @@ class SplashActvity : AppCompatActivity(), View.OnClickListener {
         val name = edtName.text.toString()
 
         if (name != "") {
+            mSecurityPreferences.storeString("name",name)
             startActivity(Intent(this, MainActivity::class.java))
         } else {
             Toast.makeText(this, "Informe um nome", Toast.LENGTH_SHORT).show()
